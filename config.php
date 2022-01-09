396e85bf8918dbbca1af7189a3779d984bd7afe6
Prosen <prosenbiswas224@gmail.com>
2022-01-09 23:48:37 +0600

    Raw file

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+    $site_url = "http://localhost/myshop";
+    $vendor_path = $site_url."/vendor";
+
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dname = "myshop";
+    
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dname);
+
+    
+    
+?>
---
+++ productlist/productdelete.php
@@ -0,0 +1,7 @@
+<?php
+    include '../config.php';
+    $vendor_delete = $_GET['vendor'];
+    $delete = "DELETE FROM `product_info` WHERE vendor='$vendor_delete'";
+    $dlt = $conn->query($delete);
+    header("Location: http://localhost/myshop");
+?>
